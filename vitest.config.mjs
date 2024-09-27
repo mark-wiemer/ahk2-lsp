51326be959a648b51cc9ee1dea4e0eb2ffe749b4
Mark Wiemer <markwiemer@outlook.com>
2024-09-26 19:26:57 -0700

    Add first unit test

---
+++ vitest.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitest/config';
+
+// https://vitest.dev/config
+// https://vitest.dev/guide/improving-performance.html
+export default defineConfig({
+	test: {
+		isolate: false,
+		pool: 'threads',
+	},
+});
