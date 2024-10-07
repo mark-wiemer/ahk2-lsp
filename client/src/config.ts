bcea9878d86440da4acd3d1626dd63296b498ba0
Mark Wiemer <markwiemer@outlook.com>
2024-10-07 11:06:56 -0700

    Get getConfigIDE fetches as well

--- client/src/config.ts
+++ client/src/config.ts
@@ -14,6 +14,7 @@ export function getConfigIDE<T = unknown>(
 	defaultValue: T,
 ): typeof defaultValue {
 	const rawResult = getConfigRoot().get<T>(key);
+	console.log('getConfigIDE', key, rawResult, defaultValue);
 	if (rawResult === undefined) return defaultValue;
 	return rawResult;
 }
