1b8d4f52fe6130b9ddb3a3695277a686c823ef47
Mark Wiemer <markwiemer@outlook.com>
2024-11-09 12:05:19 -0800

    Fix persisting config updates to property values

--- client/src/config.ts
+++ client/src/config.ts
@@ -17,3 +17,39 @@ export function getConfigIDE<T = unknown>(
 	if (rawResult === undefined) return defaultValue;
 	return rawResult;
 }
+
+export function updateConfig<T>(
+	configKey: CfgKey,
+	value: T,
+	isProperty: boolean,
+	configTarget: vscode.ConfigurationTarget | undefined = undefined,
+	outputChannel: vscode.OutputChannel | undefined = undefined,
+) {
+	const log = outputChannel?.appendLine;
+	log?.(
+		`updateConfig(${configKey}, ${JSON.stringify(value)}, ${isProperty}, ${configTarget}`,
+	);
+	/**
+	 * The key to the corresponding object for this config value.
+	 * Note that we can only update objects, not individual properties.
+	 */
+	const configObjectKey = isProperty
+		? configKey.substring(0, configKey.lastIndexOf('.'))
+		: configKey;
+	const lastKeyPart = configKey.substring(configKey.lastIndexOf('.') + 1);
+	const configRoot = getConfigRoot();
+	log?.(`Fetching ${configPrefix}.${configObjectKey}`);
+	const currentObjectValue = configRoot.get(configObjectKey, {});
+	log?.(`currentObjectValue: ${JSON.stringify(currentObjectValue)}`);
+	const newObjectValue = isProperty
+		? {
+				...currentObjectValue,
+				[lastKeyPart]: value,
+			}
+		: {
+				...currentObjectValue,
+				...value,
+			};
+	log?.(`newObjectValue: ${JSON.stringify(newObjectValue)}`);
+	configRoot.update(configObjectKey, newObjectValue, configTarget);
+}
